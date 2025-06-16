f9efe7627eca0dc32469bdaa85d08d0b4f0ee726
Mai Thanh Lam <mthanhlam111@gmail.com>
2025-06-16 17:46:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 # Thoát script khi có lỗi
 set -e
 
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm ~/miniconda3/miniconda.sh
-source ~/miniconda3/bin/activate
+mkdir -p miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3/miniconda.sh
+bash miniconda3/miniconda.sh -b -u -p miniconda3
+rm miniconda3/miniconda.sh
+source miniconda3/bin/activate
 conda init --all
 
 # Tạo môi trường Conda với Python 3.8
